ee3d793dcb8a8eeca16b5b15726d290d6a871b9a
Felipe Basilio <felipe.basilio@avenuecode.com>
2021-07-09 13:01:17 -0300

    feat: Finished chapter 1 - stage 2, useState and props

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,19 @@
+import { RepositoryItem, Counter } from "./";
+
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/unform/unform",
+};
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} />
+        <Counter />
+      </ul>
+    </section>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 // O path tem uma utilidade muito grande pois dependendo do SO o dirname pode ser indexado por '/'-mac ou por '\'-windows, dai com o path agt nao precisa especificar qual a barra correta, o __dirname com as ',' ja dao conta do recado!
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+// O React Refresh plugin serve para debuggar com maior facilidade, recarregando somente o que mudou no código e mantendo o estado dos componentes
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -17,19 +19,28 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, "public"),
+    hot: true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
-  ],
+  ].filter(Boolean),
   module: {
     //   Configura como a aplicação deve se comportar quando importar cada um dos tipos de arquivos
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
         // o babel-loader faz a integração entre o webpack e o babel
       },
       {
